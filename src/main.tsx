cfbcf2b183457e306a7c8cee05be00659029a671
Felipe Maia <felipemaiacontato1@gmail.com>
2026-04-29 01:23:20 -0300

    feat: add Table and Toast components and ToastProvider with progress bar

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client"
 import App from "./App"
 import "@/styles/index.css"
 import { ThemeProvider } from "./context/ThemeContext"
+import { ToastProvider } from "./context/ToastContext"
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <ToastProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </ToastProvider>
   </ThemeProvider>
 )
